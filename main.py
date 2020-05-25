67f6deb087c18298aeab0ac222043eae038b1d09
PRASHANT SHARMA <prashant.6251@gmail.com>
2020-05-25 05:07:10 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+user_rollno = eval(input('Please Enter your Roll Number : '))
+user_marks = eval(input('Enter your Marks : '))
+if user_marks >= 10:
+  print(f'User is pass')
+else:
+  print(f'User is fail')
+
+  
